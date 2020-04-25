e76190e1bedd4ee43b58d466cd1180fdba6de8e7
akhidrb <akhidrb@gmail.com>
2020-04-25 05:54:28 +0200

    initialize all buttons set text when button is clicked increase text size of buttons

--- app/src/main/java/com/example/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -1,17 +1,41 @@
 package com.example.tictactoe;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private static final int numOfButtons = 9;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        for (int i = 0; i < numOfButtons; i++) {
+            String buttonId = "button_" + i;
+            int resId = getResources().getIdentifier(buttonId, "id", getPackageName());
+            Button button = findViewById(resId);
+            button.setOnClickListener(this);
+        }
+
+        Button resetButton = findViewById(R.id.reset_button);
+        resetButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
 
     }
 
+
+    @Override
+    public void onClick(View v) {
+        Button button = (Button) v;
+        button.setText("X");
+    }
 
 }
