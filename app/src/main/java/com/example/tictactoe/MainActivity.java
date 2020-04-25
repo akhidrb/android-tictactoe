d94e3824eddfc785b4b079dfb88d40fb7fa78498
akhidrb <akhidrb@gmail.com>
2020-04-25 06:15:16 +0200

    apply check win based on text

--- app/src/main/java/com/example/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -7,14 +7,14 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private static final int numOfButtons = 9;
     private boolean playerOneTurn = true;
-    private List<Button> buttons = new ArrayList<>();
+    private List<Button> buttons = new LinkedList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -69,7 +69,25 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     private boolean checkWin() {
-        return true;
+        String[] fields = new String[9];
+        //get text for all buttons
+        for (int i = 0; i < 9; i++) {
+            fields[i] = buttons.get(i).getText().toString();
+        }
+        // Possible wins: 3 rows, 3 columns, 2 diagonal
+        //check rows
+        for (int i = 0; i <= 6; i+=3) {
+            if (!fields[i].isEmpty() && fields[i].equals(fields[i+1]) && fields[i].equals(fields[i+2])) return true;
+        }
+        //check columns
+        for (int i = 0; i <= 2; i++) {
+            if (!fields[i].isEmpty() && fields[i].equals(fields[i+3]) && fields[i].equals(fields[i+6])) return true;
+        }
+        //check diagonals
+        if (!fields[0].isEmpty() && fields[0].equals(fields[4]) && fields[0].equals(fields[8])) return true;
+        if (!fields[2].isEmpty() && fields[2].equals(fields[4]) && fields[2].equals(fields[6])) return true;
+
+        return false;
     }
 
 
